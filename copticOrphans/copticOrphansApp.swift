201e60c73b829966e047a6af4349a3a621e84f77
Michael Hany <121591630+MichaelHany101@users.noreply.github.com>
2025-04-28 01:51:14 +0300

    Initial Commit

---
+++ copticOrphans/copticOrphansApp.swift
@@ -0,0 +1,17 @@
+//
+//  copticOrphansApp.swift
+//  copticOrphans
+//
+//  Created by Michael Hany on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct copticOrphansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
