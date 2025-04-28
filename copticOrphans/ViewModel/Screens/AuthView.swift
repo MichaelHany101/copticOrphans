77cfefba064805e44c2c18f2ffa3c8c38ce270a9
Michael Hany <121591630+MichaelHany101@users.noreply.github.com>
2025-04-28 11:30:00 +0300

    update 1

---
+++ copticOrphans/ViewModel/Screens/AuthView.swift
@@ -0,0 +1,54 @@
+//
+//  AuthView.swift
+//  copticOrphans
+//
+//  Created by Michael Hany on 28/04/2025.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+import GoogleSignIn
+
+class AuthView : ObservableObject {
+    @Published var isSignedIn: Bool = false
+    
+    func signInWithGoogle() {
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: Application_utility.rootViewController) { user, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard
+                let user = user?.user,
+                let idToken = user.idToken else { return }
+            
+            let accessToken = user.accessToken
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken.tokenString, accessToken: accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { res, error in
+                if let error = error{
+                    print(error.localizedDescription)
+                    return
+                }
+                guard let user = res?.user else { return }
+                print(user)
+            }
+        }
+        
+    }
+    
+    func logout () async throws {
+        GIDSignIn.sharedInstance.signOut()
+        try Auth.auth().signOut()
+    }
+}
